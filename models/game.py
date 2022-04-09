220fc2a569c6edde05e0b15e646043fce342bc00
Naoufal Alaa <alaanaoufal2000@gmail.com>
2022-04-09 21:00:27 +0000

    Game seting

--- models/game.py
+++ models/game.py
@@ -1,8 +1,11 @@
+from typing import List, Optional
 from pydantic import BaseModel
+from models.question_model import QuestionModel
+from models.response_model import ResponseModel
 
 class GameModel(BaseModel):
     username : str
     quiz_id : str
-    questions : list[]
-    responses : list[] 
+    questions : List[QuestionModel]
+    responses : List[ResponseModel]
     score : int
